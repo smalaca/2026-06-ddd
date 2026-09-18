ed886e6904ccc728282b3c5c0d1b0d4d0e52c53f
smalaca <sebastian.malaca@gmail.com>
2026-09-18 10:06:11 +0200

    move registering attendance from OfferApplicationService to TrainingApplicationService

--- src/main/java/com/smalaca/trainingcenter/opentrainings/application/training/TrainingApplicationService.java
+++ src/main/java/com/smalaca/trainingcenter/opentrainings/application/training/TrainingApplicationService.java
@@ -1,5 +1,7 @@
 package com.smalaca.trainingcenter.opentrainings.application.training;
 
+import com.smalaca.trainingcenter.opentrainings.domain.offer.Offer;
+import com.smalaca.trainingcenter.opentrainings.domain.offer.OfferRepository;
 import com.smalaca.trainingcenter.opentrainings.domain.training.*;
 import com.smalaca.trainingcenter.opentrainings.domain.training.commands.AddTrainingDomainCommand;
 
@@ -7,9 +9,11 @@ import java.util.UUID;
 
 public class TrainingApplicationService {
     private final TrainingRepository trainingRepository;
+    private final OfferRepository offerRepository;
 
-    public TrainingApplicationService(TrainingRepository trainingRepository) {
+    public TrainingApplicationService(TrainingRepository trainingRepository, OfferRepository offerRepository) {
         this.trainingRepository = trainingRepository;
+        this.offerRepository = offerRepository;
     }
 
     public UUID addTraining(AddTrainingCommand command) {
@@ -23,5 +27,13 @@ public class TrainingApplicationService {
         Training training = Training.create(domainCommand);
 
         return trainingRepository.save(training);
+    }
+
+    public UUID registerAttendance(UUID trainingId) {
+        TrainingId trainingIdVO = new TrainingId(trainingId);
+
+        Offer offer = Offer.create(trainingIdVO);
+
+        return offerRepository.save(offer);
     }
 }
