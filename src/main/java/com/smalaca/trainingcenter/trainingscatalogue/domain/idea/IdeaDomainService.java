19e7c577150eedec8abbeb3322fea47725f2a820
smalaca <sebastian.malaca@gmail.com>
2026-09-18 13:49:13 +0200

    add 1-3 options

--- src/main/java/com/smalaca/trainingcenter/trainingscatalogue/domain/idea/IdeaDomainService.java
+++ src/main/java/com/smalaca/trainingcenter/trainingscatalogue/domain/idea/IdeaDomainService.java
@@ -13,8 +13,17 @@ public class IdeaDomainService {
     }
 
     public AcceptIdeaResponse acceptIdea(Idea idea, IdeaId ideaId, ReviewerId reviewerId) {
+        // opcja 1
         Draft draft = idea.accept(reviewerId, trainersCatalogue);
         IdeaAcceptedEvent event = IdeaAcceptedEvent.create(ideaId, reviewerId);
+
+        // opcja 2
+//        IdeaAcceptedEvent event = idea.accept(reviewerId, trainersCatalogue);
+//        Draft draft = Draft.create(event);
+
+        // opcja 3 - unikamy
+//        Draft draft = idea.accept(reviewerId, trainersCatalogue);
+//        IdeaAcceptedEvent event = IdeaAcceptedEvent.create(ideaId, reviewerId, idea.getTitle(), idea.getDescription());
 
         return new AcceptIdeaResponse(idea, draft, event);
     }
