d70469bbaf2beaa2cfe926eb20e49d449fc8e16a
smalaca <sebastian.malaca@gmail.com>
2026-09-18 11:27:05 +0200

    add comment with request about trainer

--- src/main/java/com/smalaca/trainingcenter/trainingscatalogue/domain/idea/Idea.java
+++ src/main/java/com/smalaca/trainingcenter/trainingscatalogue/domain/idea/Idea.java
@@ -37,6 +37,8 @@ public class Idea {
     // Factory
     // opcja 2 - external service
     public Draft accept(ReviewerId reviewerId, TrainersCatalogue trainersCatalogue) {
+//        TrainerCompetencyRequest request = new TrainerCompetencyRequest(title, reviewerId);
+
         if (trainersCatalogue.isNonCompetent(reviewerId)) {
             throw DraftException.nonCompetentReviewer(reviewerId);
         }
